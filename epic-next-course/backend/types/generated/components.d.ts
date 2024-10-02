ce76ff645061afde3b142208d3671e63b9259fba
Paul Bratslavsky <paulbratslavsky@Pauls-MacBook-Pro.local>
2024-10-01 19:07:23 -0500

    part two update

--- epic-next-course/backend/types/generated/components.d.ts
+++ epic-next-course/backend/types/generated/components.d.ts
@@ -1,3 +1,36 @@
-/*
- * The app doesn't have any components yet.
- */
+import type { Struct, Schema } from '@strapi/strapi';
+
+export interface LayoutHeroSection extends Struct.ComponentSchema {
+  collectionName: 'components_layout_hero_sections';
+  info: {
+    displayName: 'Hero Section';
+    description: '';
+  };
+  attributes: {
+    image: Schema.Attribute.Media<'images'>;
+    heading: Schema.Attribute.String;
+    subHeading: Schema.Attribute.Text;
+    link: Schema.Attribute.Component<'components.link', false>;
+  };
+}
+
+export interface ComponentsLink extends Struct.ComponentSchema {
+  collectionName: 'components_components_links';
+  info: {
+    displayName: 'Link';
+  };
+  attributes: {
+    url: Schema.Attribute.String;
+    text: Schema.Attribute.String;
+    isExternal: Schema.Attribute.Boolean & Schema.Attribute.DefaultTo<false>;
+  };
+}
+
+declare module '@strapi/strapi' {
+  export module Public {
+    export interface ComponentSchemas {
+      'layout.hero-section': LayoutHeroSection;
+      'components.link': ComponentsLink;
+    }
+  }
+}
